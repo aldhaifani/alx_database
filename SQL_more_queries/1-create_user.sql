14469ea0bdf5bb50d964f8e947aeb0cec7d5ebda
Tareq <tareqaldhaifani@gmail.com>
2023-09-01 11:41:26 +0400

    task 1 - granted all privilages

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- a script that creates the MySQL server user user_0d_1.
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' IF NOT EXISTS;
+GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILAGES;
